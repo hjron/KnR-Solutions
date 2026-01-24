5bcf685fca1a740a5c42b5d02ecbddcc4c710e68
ron h <hjron@protonmail.com>
2026-01-24 11:35:43 -0600

    character copy 2d version, save 36b on file, but executable is same size

--- ch01/ccopy.c
+++ ch01/ccopy.c
@@ -7,11 +7,8 @@
 int main(int argc, char *argv[]) {
     int c;
 
-    c = getchar();
-    while (c != EOF) {
+    while ((c = getchar()) != EOF)
         putchar(c);
-        c = getchar();
-    }
 
     return 0;
 }
