1bb7a2fa8c48cb8624105082954d02d237fd7595
ron h <hjron@protonmail.com>
2026-01-24 11:11:05 -0600

    use symbolic constants

--- ch01/ftoc.c
+++ ch01/ftoc.c
@@ -3,19 +3,12 @@
 /* print Fahrenheit to Celsius table for
  * fahr = 0, 20, ..., 300 */
 
+#define LOWER 0         /* lower limit for table */
+#define UPPER 300       /* upper limit for table */
+#define STEP 20         /* step size */
+
 int main(int argc, char *argv[]) {
-    float fahr, celsius;
-    int lower, upper, step;
-
-    lower = 0;
-    upper = 300;
-    step = 20;
-
-    fahr = lower;
-    while (fahr <= upper) {
-        celsius = 5.0 / 9.0 * (fahr - 32.0);
-        printf("%3.0f %6.1f\n", fahr, celsius);
-        fahr += step;
-    }
+    for (int fahr = LOWER; fahr <= UPPER; fahr += STEP)
+        printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
     return 0;
 }
