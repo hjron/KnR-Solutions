2fb2e1b501579bf0af39156744f0634a67a9a0c9
ron h <hjron@protonmail.com>
2026-02-10 17:26:30 -0600

    finish ch03 itoa example (with reverse)

--- ch03/itoa.c
+++ ch03/itoa.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
+#include <string.h>
 
 void itoa(int n, char s[]);
+void reverse(char s[]);
 
 int main(int argc, char *argv[]) {
+    int n = 123456;
+    char s[20];
+    itoa(n, s);
+
+    printf("%d converted to string: \"%s\"\n", n, s);
     return 0;
 }
 
@@ -10,4 +17,25 @@ int main(int argc, char *argv[]) {
 void itoa(int n, char s[]) {
     int i, sign;
 
-    if ((sign = n) < 0)
+    if ((sign = n) < 0) /* record sign */
+        n = -n;         /* make n positive */
+    i = 0;
+    do {                /* generate digits in reverse order */
+        s[i++] = n % 10 + '0';  /* get next digit */
+    } while ((n /= 10) > 0);    /* delete it */
+    if (sign < 0)
+        s[i] = '-';
+    s[i] = '\0';
+    reverse(s);
+}
+
+/* reverse: reverse string s in place */
+void reverse(char s[]) {
+    int c, i, j;
+
+    for (i = 0, j = strlen(s) - 1; i < j; i++, j--) {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
+}
